bf02b406ac31e66f1eb3df9611044456c2fb5549
enolgor <enolgor@teleco.upv.es>
2019-02-17 22:31:13 +0000

    Updates

--- default.config.frontend.js
+++ default.config.frontend.js
@@ -1,4 +1,5 @@
 export default {
   graphqlUrl: () => `${window.location.protocol}//${window.location.host}/graphql`,
   subscriptionUrl: () => `ws://${window.location.host}/subscriptions`,
+  inactiveStationThresholdMillis: 30 * 60 * 1000,
 };
